8a17d8c363fb06e14396a3936e08e5eafdc6169b
Andrew Kanner <andrew.kanner@gmail.com>
2014-08-07 00:11:40 +0400

    added Makefile.bat example for Windows texlive2014

---
+++ Makefile.bat
@@ -0,0 +1,4 @@
+C:\texlive\2014\bin\win32\pdflatex.exe dissertation.tex
+C:\texlive\2014\bin\win32\bibtex.exe dissertation
+C:\texlive\2014\bin\win32\pdflatex.exe dissertation.tex
+C:\texlive\2014\bin\win32\pdflatex.exe dissertation.tex
