2b25be1ac09fa712f5cd714c8542cc79ef05662d
Andrew Kanner <andrew.kanner@gmail.com>
2017-04-19 15:55:07 +0300

    Makefile*.bat: refactoring in batch files build sequence

--- Makefile-clean.bat
+++ Makefile-clean.bat
@@ -1,14 +1,6 @@
 @echo off
 
-del *.aux *.log *.ps *.lof *.lot *.out *.toc *.pdf *.blg *.bbl *.nls *.nlo *.ilg || goto :error
-::del %path%\contrib\*.aux || goto :error
-del parts\*.aux parts\*.bbl parts\*.blg || goto :error
-del synopsis-parts\*.aux synopsis-parts\*.bbl synopsis-parts\*.blg || goto :error
-
-:: succeeded
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%. Check the error messages above...
-::pause
-exit /b %errorlevel%
+del *.aux *.log *.ps *.lof *.lot *.out *.toc *.pdf *.blg *.bbl *.nls *.nlo *.ilg
+::del %path%\contrib\*.aux
+del parts\*.aux parts\*.bbl parts\*.blg
+del synopsis-parts\*.aux synopsis-parts\*.bbl synopsis-parts\*.blg
--- Makefile.bat
+++ Makefile.bat
@@ -1,41 +1,46 @@
 @echo off
 
-set texlive_bin=C:\texlive\2014\bin\win32
-::set texlive_bin=D:\programs\texlive\2016\bin\win32
+::set texlive_bin=C:\texlive\2014\bin\win32\
+::set texlive_bin=D:\programs\texlive\2016\bin\win32\
+:: it should work if LaTeX distribution is correctly set in %PATH%
+set texlive_bin=
 :: set perl_bin - only works with GitHub client installed
 for /f "delims=" %%i in ('where /R %LOCALAPPDATA%\GitHub perl') do set perl_bin=%%i
 
 :: set path for local or template-usecase
-if "%~1"=="" set path=.
-else set path=%~1
+if "%~1"=="" (
+	set mypath=.
+) else (
+	set mypath=%~1
+)
 
-set dissertation=%path%\dissertation
+set dissertation=%mypath%\dissertation
 set dissertation-bib=dissertation
-set synopsis=%path%\synopsis
+set synopsis=%mypath%\synopsis
 set synopsis-bib=synopsis
-set booklet=%path%\booklet
+set booklet=%mypath%\booklet
 
 :: dissertation
-%texlive_bin%\pdflatex.exe %dissertation%.tex || goto :error
-%texlive_bin%\bibtex.exe %dissertation-bib% || goto :error
-%perl_bin% %path%\contrib\bbl-sorter.pl || goto :error
-%texlive_bin%\makeindex.exe %dissertation-bib%.nlo -s nomencl.ist -o %dissertation-bib%.nls || goto :error
-%texlive_bin%\pdflatex.exe %dissertation%.tex || goto :error
+%texlive_bin%pdflatex.exe %dissertation%.tex || goto :error
+%texlive_bin%bibtex.exe %dissertation-bib% || goto :error
+%perl_bin% %mypath%\contrib\bbl-sorter.pl || goto :error
+%texlive_bin%makeindex.exe %dissertation-bib%.nlo -s nomencl.ist -o %dissertation-bib%.nls || goto :error
+%texlive_bin%pdflatex.exe %dissertation%.tex || goto :error
 :: some elements (bib, nom) could not be build
-%texlive_bin%\pdflatex.exe %dissertation%.tex || goto :error
-%texlive_bin%\pdflatex.exe %dissertation%.tex || goto :error
+%texlive_bin%pdflatex.exe %dissertation%.tex || goto :error
+%texlive_bin%pdflatex.exe %dissertation%.tex || goto :error
 
 :: synopsis
-%texlive_bin%\pdflatex.exe %synopsis%.tex || goto :error
-%texlive_bin%\bibtex.exe %synopsis-bib%1 || goto :error
-%texlive_bin%\bibtex.exe %synopsis-bib%2 || goto :error
-%texlive_bin%\bibtex.exe %synopsis-bib%3 || goto :error
-%texlive_bin%\pdflatex.exe %synopsis%.tex || goto :error
+%texlive_bin%pdflatex.exe %synopsis%.tex || goto :error
+%texlive_bin%bibtex.exe %synopsis-bib%1 || goto :error
+%texlive_bin%bibtex.exe %synopsis-bib%2 || goto :error
+%texlive_bin%bibtex.exe %synopsis-bib%3 || goto :error
+%texlive_bin%pdflatex.exe %synopsis%.tex || goto :error
 :: some elements (bib) could not be build
-%texlive_bin%\pdflatex.exe %synopsis%.tex || goto :error
+%texlive_bin%pdflatex.exe %synopsis%.tex || goto :error
 
 :: booklet
-%texlive_bin%\pdflatex.exe %booklet%.tex || goto :error
+%texlive_bin%pdflatex.exe %booklet%.tex || goto :error
 
 :: succeeded
 goto :EOF
