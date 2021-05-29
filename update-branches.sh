3ecdaf194ad93905c78a0b5df9a2da8a099bb22c
Andrew Kanner <andrew.kanner@gmail.com>
2021-05-29 15:39:50 +0300

    added script to update branches on changes in master

---
+++ update-branches.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+# branches should be local
+#git fetch
+#git checkout remotes/origin/texstudio-template && git checkout -b texstudio-template
+#git checkout remotes/origin/texlive-2014 && git checkout -b texlive-2014
+#git checkout remotes/origin/texlive-2018-fancyhdr && git checkout -b texlive-2018-fancyhdr
+#git checkout remotes/origin/texlive-2018+texstudio && git checkout -b texlive-2018+texstudio
+#git checkout remotes/origin/texlive-2018+texstudio-stperl && git checkout -b texlive-2018+texstudio-stperl
+# make changes in master
+git checkout texstudio-template && git rebase master
+git checkout texlive-2014 && git rebase master
+git checkout texlive-2018-fancyhdr && git rebase master
+git checkout texlive-2018+texstudio && git rebase texlive-2018-fancyhdr
+git checkout texlive-2018+texstudio-stperl && git rebase texlive-2018+texstudio
+git checkout master
+git push -f && git push -f okbrepo
