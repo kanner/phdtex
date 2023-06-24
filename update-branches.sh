1670a20ff161bc9053f3ee435d986d95629bde98
Andrew Kanner <andrew.kanner@gmail.com>
2023-06-24 18:40:19 +0300

    update-branches: disable opp1_5 branch update
    Signed-off-by: Andrew Kanner <andrew.kanner@gmail.com>

--- update-branches.sh
+++ update-branches.sh
@@ -6,14 +6,14 @@ set -ex
 #git checkout remotes/origin/texstudio-template && git checkout -b texstudio-template
 #git checkout remotes/origin/texlive-2014 && git checkout -b texlive-2014
 #git checkout remotes/origin/texlive-2018-fancyhdr && git checkout -b texlive-2018-fancyhdr
-#git checkout remotes/origin/texlive-2018-fancyhdr-opp1_5 && git checkout -b texlive-2018-fancyhdr-opp1_5
+##git checkout remotes/origin/texlive-2018-fancyhdr-opp1_5 && git checkout -b texlive-2018-fancyhdr-opp1_5
 #git checkout remotes/origin/texlive-2018+texstudio && git checkout -b texlive-2018+texstudio
 #git checkout remotes/origin/texlive-2018+texstudio-stperl && git checkout -b texlive-2018+texstudio-stperl
 # make changes in master
 git checkout texstudio-template && git rebase master
 git checkout texlive-2014 && git rebase master
 git checkout texlive-2018-fancyhdr && git rebase master
-git checkout texlive-2018-fancyhdr-opp1_5 && git rebase master
+#git checkout texlive-2018-fancyhdr-opp1_5 && git rebase master
 git checkout texlive-2018+texstudio && git rebase texlive-2018-fancyhdr
 git checkout texlive-2018+texstudio-stperl && git rebase texlive-2018+texstudio
 git checkout master
